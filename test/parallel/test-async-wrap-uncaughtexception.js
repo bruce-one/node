a113609397e996c4c51ac57a60ad24d4988a2dc2
Anna Henningsen <anna@addaleax.net>
2017-11-02 20:06:49 +0100

    debug

--- test/parallel/test-async-wrap-uncaughtexception.js
+++ test/parallel/test-async-wrap-uncaughtexception.js
@@ -10,9 +10,10 @@ const call_log = [0, 0, 0, 0];  // [before, callback, exception, after];
 let call_id = null;
 let hooks = null;
 
-
+process._rawDebug(`Adding beforeExitHandler`)
 process.on('beforeExit', common.mustCall(() => {
-  process.removeAllListeners('uncaughtException');
+  process._rawDebug(`beforeExit`, (new Error()).stack)
+  //process.removeAllListeners('uncaughtException');
   hooks.disable();
   assert.strictEqual(typeof call_id, 'number');
   assert.deepStrictEqual(call_log, [1, 1, 1, 1]);
@@ -21,26 +22,33 @@ process.on('beforeExit', common.mustCall(() => {
 
 hooks = async_hooks.createHook({
   init(id, type) {
+    process._rawDebug('init', id, type)
     if (type === 'RANDOMBYTESREQUEST')
       call_id = id;
   },
   before(id) {
+    process._rawDebug('before')
     if (id === call_id) call_log[0]++;
   },
   after(id) {
+    process._rawDebug('after')
     if (id === call_id) call_log[3]++;
   },
 }).enable();
 
 
-process.on('uncaughtException', common.mustCall(() => {
+process.on('uncaughtException', common.mustCall((err) => {
+  process._rawDebug(`uncaught exception!!`, err, (new Error()).stack)
   assert.strictEqual(call_id, async_hooks.executionAsyncId());
   call_log[2]++;
 }));
 
 
+process._rawDebug(`scheduling work`)
 require('crypto').randomBytes(1, common.mustCall(() => {
+  process._rawDebug(`enter callback`)
   assert.strictEqual(call_id, async_hooks.executionAsyncId());
   call_log[1]++;
-  throw new Error();
+  process._rawDebug(`exit callback [throw]`)
+  throw new Error('foo');
 }));
