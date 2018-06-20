39d40c87cceadab09672c5041f0efeb99f374175
Anna Henningsen <anna@addaleax.net>
2018-06-20 17:46:03 +0200

    Revert "esm: ensure require.main for CJS top-level loads"
    This reverts commit 8ee604ca71dfa459ec2fe00495aeb51e12a20af1
    due to a regression when loading a CJS main script in ESM mode.
    
    Minor merge conflict cleanup was necessary for
    `lib/internal/modules/esm/module_job.js`.
    
    Refs: https://github.com/nodejs/node/pull/21150

--- test/es-module/test-esm-cjs-main.js
+++
@@ -1,6 +0,0 @@
-// Flags: --experimental-modules
-'use strict';
-require('../common');
-const assert = require('assert');
-exports.asdf = 'asdf';
-assert.strictEqual(require.main.exports.asdf, 'asdf');
