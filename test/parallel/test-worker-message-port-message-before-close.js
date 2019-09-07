76fd0b2eadcae16c992cf6bd518f41856979bf12
Anna Henningsen <anna@addaleax.net>
2019-09-07 19:48:10 +0200

    Debugging

--- test/parallel/test-worker-message-port-message-before-close.js
+++ test/parallel/test-worker-message-port-message-before-close.js
@@ -17,26 +17,32 @@ const { Worker, MessageChannel } = require('worker_threads');
 // - Thread 2 closes the port, discarding message B in the process.
 
 const base = process.hrtime.bigint()
+const logs = [];
 async function test() {
   const worker = new Worker(`
   require('worker_threads').parentPort.on('message', ({ port }) => {
-    process._rawDebug('worker got message, sending', process.hrtime.bigint() - ${base}n);
     port.postMessage('firstMessage');
     port.postMessage('lastMessage');
     port.close();
   });
   `, { eval: true });
+  worker.unref();
 
   for (let i = 0; i < 10000; i++) {
     const { port1, port2 } = new MessageChannel();
     worker.postMessage({ port: port2 }, [ port2 ]);
     assert.deepStrictEqual(await once(port1, 'message'), ['firstMessage']);
-    process._rawDebug(i, 'got firstMessage', process.hrtime.bigint() - base);
+    logs.push([i, 'got firstMessage', process.hrtime.bigint() - base]);
     assert.deepStrictEqual(await once(port1, 'message'), ['lastMessage']);
-    process._rawDebug(i, 'got lastMessage', process.hrtime.bigint() - base);
+    logs.push([i, 'got lastMessage', process.hrtime.bigint() - base]);
   }
 
   await worker.terminate();
+  logs.push([-1, 'done', process.hrtime.bigint() - base]);
 }
 
 test().then(common.mustCall());
+
+setTimeout(() => {
+  console.log(logs.join('\n'))
+}, 10000).unref();
